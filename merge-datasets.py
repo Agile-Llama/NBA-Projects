6a876375e64840ab89ca855fe5727efe7a84a5cb
Chase Hagger <agilellama@gmail.com>
2020-09-10 10:47:23 +1200

    updated readme.md and scripts

--- merge-datasets.py
+++ merge-datasets.py
@@ -8,16 +8,21 @@ df_advanced = pd.read_csv("Data/advanced.csv")
 df_extra = pd.read_csv("Data/extra.csv")
 
 # Merge the three datasets together. Also drop the columns which aren't relevant to our goal.
-def merge(df_per_36, df_advanced, df_extra):
+def merge_clean(df_per_36, df_advanced, df_extra):
     df_per_36 = df_per_36.drop(['Age', 'Tm', 'G', 'GS', 'MP'], axis = 1)
     df_advanced = df_advanced.drop(['Rk', 'Age', 'G', 'MP', 'Pos', 'Tm'], axis = 1)
+
+    # Remove special characters
+    df_per_36['Player'] = df_per_36.Player.str.replace('.','')
+    df_per_36['PlayerAdvanced'] = df_advanced.PlayerAdvanced.str.replace('.','')
+
 
     # Merging 36_minutes.csv and advanced are easy. They are both from the same place so they match up well.
     # Merging extras.csv will be more annoying.
     df = pd.concat([df_per_36, df_advanced], axis=1)
 
     # Drop the duplicate of the player name. And the rank which isn't relevant.
-    df = df.drop(['Player-Advanced', 'Rk'], axis = 1)
+    df = df.drop(['PlayerAdvanced', 'Rk'], axis = 1)
 
     # Merge based off a key. That key is the player name.
     final_merge = pd.merge(left=df, right=df_extra, left_on='Player', right_on='Player')
@@ -27,7 +32,8 @@ def merge(df_per_36, df_advanced, df_extra):
 
     return final_merge
 
+
 if __name__ == '__main__':
-    df = merge(df_per_36, df_advanced, df_extra)  
+    df = merge_clean(df_per_36, df_advanced, df_extra)  
     df.to_csv('Data/final.csv') 
 
