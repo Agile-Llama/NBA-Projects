6400feeb0240000536693008262062c7c9775b86
Chase Hagger <agilellama@gmail.com>
2020-09-14 13:09:10 +1200

    pca working no vorp though

--- pca.py
+++ pca.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import config
 from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import numpy as np
+
+# https://github.com/Phlya/adjustText
+from adjustText import adjust_text
 
 df = pd.read_csv(config.final_dataset)
 
@@ -11,13 +16,21 @@ y = df['Pos']
 x = df
 x = x.drop(['Pos','Player', 'G', 'MP', 'Index'], axis=1)
 
+def clean_dataset(df):
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    return df[indices_to_keep].astype(np.float64)
+
+x = clean_dataset(x)
+
 x = StandardScaler().fit_transform(x)
 
 pca = PCA(n_components=2)
-principalComponents = pca.fit_transform(x)
-principalDf = pd.DataFrame(data = principalComponents , columns = ['principal component 1', 'principal component 2'])
+principal_components = pca.fit_transform(x)
+principal_df = pd.DataFrame(data = principal_components , columns = ['principal component 1', 'principal component 2'])
 
-finalDf = pd.concat([principalDf, df[['Pos']]], axis = 1)
+final_df = pd.concat([principal_df, df[['Pos']]], axis = 1)
 
 fig = plt.figure(figsize = (8,8))
 ax = fig.add_subplot(1,1,1) 
@@ -27,21 +40,27 @@ ax.set_title('2 component PCA', fontsize = 20)
 targets = ['C', 'PF', 'PG', 'SF', 'SG']
 colors = ['r', 'g', 'b', 'c', 'm']
 
-for target, color in zip(targets,colors):
-    indicesToKeep = finalDf['Pos'] == target
-    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1'], finalDf.loc[indicesToKeep, 'principal component 2'], c = color, s = 50)
+text = []
+
+for target, color in zip(targets, colors):
+    indices_to_keep = final_df['Pos'] == target
+    ax.scatter(final_df.loc[indices_to_keep, 'principal component 1'], final_df.loc[indices_to_keep, 'principal component 2'], c = color, s = 50, cmap=plt.cm.Blues)
     
-    # ax.text(finalDf.loc[indicesToKeep, 'principal component 1'], finalDf.loc[indicesToKeep, 'principal component 2'], s='hello', fontsize=10)
-
 ax.legend(targets)
-# ax.grid()
+ax.grid()
 
+# Function which annotates each point on the PCA plot with the players name. Currently just adds all to a list. 
+# This list is used to add labels later without overlapping labels.
 def label_point(x, y, val, ax):
     a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
     for i, point in a.iterrows():
-        ax.text(point['x'], point['y']+0.1, str(point['val']))
+        #ax.text(point['x'], point['y']+0.1, str(point['val']))
+        text.append(plt.text(point['x'], point['y']+0.1, str(point['val']), size=9))
 
-label_point(finalDf['principal component 1'], finalDf['principal component 2'], df['Player'], ax)
 
+label_point(final_df['principal component 1'], final_df['principal component 2'], df['Player'], ax)
+
+# Add the labels to the plots. Using a library which reduces overlapping.
+plt.title('(%s Interations) Overall Player Impact' % adjust_text(text, arrowprops=dict(arrowstyle="-", color='k', lw=0.5), save_steps=False))
 
 plt.show()
