1363a21dca4b1c8f14b2820f1ff901b5595916ff
Chase Hagger <agilellama@gmail.com>
2021-06-20 13:29:00 +1200

    removed "old" folder from repo

--- Old/config.py
+++
@@ -1,5 +0,0 @@
-dataset_directory = '/Users/agilellama/Desktop/Nba-Visualization/Data-Sets/NBA-Season-Dataset-1979-2016.csv'
-
-year_directory = '/Users/agilellama/Desktop/Nba-Visualization/Data-Sets/Year-Datasets'
-
-single_year_dataset_directory = '/Users/agilellama/Desktop/Nba-Visualization/Data-Sets/17-18-Nba-Dataset.csv'
